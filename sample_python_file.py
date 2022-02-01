aca3ab485c442abc7a72f88108c5a64ba8bcec04
Sanushana Kanthavel <shanu.29@hotmail.com>
2022-02-01 18:48:57 -0500

    Update sample_python_file.py
    Added Informations about Sanushana

--- sample_python_file.py
+++ sample_python_file.py
@@ -1,2 +1,13 @@
 print("Hello World")
 print("Hi Guys, I am learning git")
+
+
+#Sanushana
+def Sanushana():
+    x = int(input("enter first number: "))
+    y = int(input("enter second number: "))
+    total = x + y
+    print("total:", total)
+
+Sanushana()
+
