3ae16dbd1cb5830ce10e66dbc38c6892c03e0c74
jucelisa <elianamariac@gmail.com>
2022-02-02 10:05:03 -0500

    add changes Eliana.py

--- Eliana.py
+++ Eliana.py
@@ -1,17 +1,17 @@
 import sys
-import pytest
+# import pytest
 
-number1=float(input("Enter first number: "))
-number2=float(input("Enter second number: "))
+number1=float(input("Enter first number sum: "))
+number2=float(input("Enter second number sum: "))
 def sum(a,b):
-    sum=a/b
+    sum=a+b
     return sum
 print("Result is: ",sum(number1,number2))
 
 # #========================================================
 
-number1=float(input("Enter first number: "))
-number2=float(input("Enter second number: "))
+number1=float(input("Enter first number to div: "))
+number2=float(input("Enter second number to div: "))
 def division(a,b):
     div=a/b
     return div
@@ -19,14 +19,19 @@ print("Result is: ",division(number1,number2))
 
 # ===========================================================
 
-number1=float(input("Enter first number: "))
-number2=float(input("Enter second number: "))
+number1=float(input("Enter first number to sus: "))
+number2=float(input("Enter second number to sus: "))
 def sustration(a,b):
-    sus=a/b
+    sus=a-b
     return sus
 print("Result is: ",sustration(number1,number2))
 
 # ===========================================================
 
-
+number1=float(input("Enter first number to mul: "))
+number2=float(input("Enter second number to mul: "))
+def multiplication(a,b):
+    mul=a*b
+    return mul
+print("Result is: ",multiplication(number1,number2))
 
