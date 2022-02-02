fd0c401c52c153b09f617f2cbd5ab4b7fb037c85
kpatel2743 <kpatel2743@gmail.com>
2022-02-01 19:21:13 -0500

    Made changes by Keval

--- sample_python_file.py
+++ sample_python_file.py
@@ -1,2 +1,11 @@
 print("Hello World")
 print("Hi Guys, I am learning git")
+
+# changes made by Keval
+#python program to subtract two numbers using function
+def Keval(x,y):
+    sub=x-y
+    return sub
+num1=int(input("please enter first number: "))
+num2=int(input("please enter second number: "))
+print("Subtraction is: ",Keval(num1,num2))
