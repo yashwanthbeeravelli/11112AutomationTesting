9ea1d86170cad5ac5450b4dd3281333c88bf45d9
jucelisa <elianamariac@gmail.com>
2022-02-01 20:55:55 -0500

    New file.python

---
+++ Eliana.py
@@ -0,0 +1,24 @@
+import sys
+import pytest
+
+number1=float(input("Enter first number: "))
+number2=float(input("Enter second number: "))
+def sum(a,b):
+    sum=a/b
+    return sum
+print("Result is: ",sum(number1,number2))
+
+# #========================================================
+
+number1=float(input("Enter first number: "))
+number2=float(input("Enter second number: "))
+def division(a,b):
+    div=a/b
+    return div
+print("Result is: ",division(number1,number2))
+
+# ===========================================================
+
+
+
+
