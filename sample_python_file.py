03d8b7273a88c219c2aabfb4c2f9edea99dfe857
Yashwanth <yashwanthbeeravelli@gmail.com>
2022-02-01 21:21:12 -0500

    Arranged the code and functions properly

--- sample_python_file.py
+++ sample_python_file.py
@@ -1,30 +1,28 @@
 print("Hello World")
 print("Hi Guys, I am learning git")
 
-# changes made by Keval
-#python program to subtract two numbers using function
-def Keval(x,y):
-    sub=x-y
-    return sub
-num1=int(input("please enter first number: "))
-num2=int(input("please enter second number: "))
-print("Subtraction is: ",Keval(num1,num2))
 
-#Sanushana
+# changes made by Keval
+# python program to subtract two numbers using function
+def Keval(x, y):
+    sub = x - y
+    return sub
+
+
+num1 = int(input("please enter first number: "))
+num2 = int(input("please enter second number: "))
+print("Subtraction is: ", Keval(num1, num2))
+
+
+# Sanushana
 def Sanushana():
     x = int(input("enter first number: "))
     y = int(input("enter second number: "))
     total = x + y
     print("total:", total)
 
+
 Sanushana()
-
-#Function2
-a = int(input("enter the first number"))
-b = int(input("enter the second number"))
-
-c = a*b
-print("the output is:c")
 
 
 def Sanu():
@@ -33,5 +31,16 @@ def Sanu():
     total = x - y
     print("total:", total)
 
+
 Sanu()
->>>>>>> 26006c35a82497cb7caffb4bf71932ff995546e4
+
+
+# Function2
+def girish():
+    a = int(input("enter the first number"))
+    b = int(input("enter the second number"))
+
+    c = a * b
+    print("the output is:c")
+
+girish()
