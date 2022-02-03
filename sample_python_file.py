d5b64d9648add92fd354966afec9b262487cc755
jucelisa <elianamariac@gmail.com>
2022-02-03 08:25:45 -0500

    add to sample python

--- sample_python_file.py
+++ sample_python_file.py
@@ -39,8 +39,47 @@ Sanu()
 def girish():
     a = int(input("enter the first number"))
     b = int(input("enter the second number"))
-
     c = a * b
     print("the output is:c")
 
 girish()
+
+# =================Eliana========================
+import sys
+# import pytest
+
+number1=float(input("Enter first number to sum: "))
+number2=float(input("Enter second number to sum: "))
+def sum(a,b):
+    sum=a+b
+    return sum
+print("Result is: ",sum(number1,number2))
+
+# #========================================================
+
+number1=float(input("Enter first number to div: "))
+number2=float(input("Enter second number to div: "))
+def division(a,b):
+    div=a/b
+    return div
+print("Result is: ",division(number1,number2))
+
+# ===========================================================
+
+number1=float(input("Enter first number to sus: "))
+number2=float(input("Enter second number to sus: "))
+def sustration(a,b):
+    sus=a-b
+    return sus
+print("Result is: ",sustration(number1,number2))
+
+# ===========================================================
+
+number1=float(input("Enter first number to mul: "))
+number2=float(input("Enter second number to mul: "))
+def multiplication(a,b):
+    mul=a*b
+    return mul
+print("Result is: ",multiplication(number1,number2))
+
+# ==========================Eliana===========================
